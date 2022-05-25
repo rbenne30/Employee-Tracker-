ae8277b5acfb38807529ad68c5c0f4e34f7f3ec4
rbenne30 <ryannebennett1@outlook.com>
2022-05-25 15:20:28 -0400

    added inquirer

--- server.js
+++ server.js
@@ -1,33 +1,19 @@
-const express = require('express');
-const PORT = process.env.PORT || 3001;
-const app = express();
-const mysql = require('mysql2');
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-
-
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-    res.status(404).end();
-  });
-
-// Connect to database
-const db = mysql.createConnection(
+require('dotenv')
+const inquirer = require('inquirer')
+const mysql = require('mysql2/promise')
+inquirer
+  .prompt([
     {
-      host: 'localhost',
-      // Your MySQL username,
-      user: 'root',
-      // Your MySQL password
-      password: 'password',
-      database: 'employee_db'
+      name: "department",
+      type: "input",
+      message: "What would you like to do?"
     },
-    console.log('Connected to the employee_db database.')
-  );
-  
-
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });  
+    {
+      name: "doing",
+      type: "input",
+      message: "what is the name of the department?"
+    }
+  ])
+  .then(function (answers) {
+    console.log(answers)
+  })
