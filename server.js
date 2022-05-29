732e7cca8aca1cee6e05bbbcdf35538234292a8e
rbenne30 <ryannebennett1@outlook.com>
2022-05-29 17:29:19 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,24 +1,79 @@
-require('dotenv')
+require('dotenv').config()
+require('console.table')
 const inquirer = require('inquirer')
 const mysql = require('mysql2/promise')
-inquirer
-  .prompt([
-    {
-      name: "department",
-      type: "input",
-      message: "What would you like to do?"
-    },
-    {
-      name: "doing",
-      type: "input",
-      message: "what is the name of the department?"
-    }
-  ])
-  .then(function (answers) {
-    console.log(answers)
-    // .then((employee_db))
-    // res.json(employee_db);
-  // }) .catch((err) => {
-    // res.json(err);
-  // });
+
+mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+})
+  .catch((err) => {
+    console.error(err)
   })
+  .then((connection) => {
+    if (!connection) {
+      return;
+    }
+    console.log("Connected to database");
+    return promptLoop(connection)
+      .then(() => {
+        connection.end();
+      })
+      .catch((err) => {
+        console.error(err)
+        connection.end();
+      })
+  })
+  .then(() => {
+    console.log("Thank you for using this program");
+  })
+
+const promptLoop = (connection) => {
+  return inquirer
+    .prompt([
+      {
+        name: "choice",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View Departments",
+          "View Roles",
+          "View Employees",
+          "Add a Department",
+          "Add a role",
+          "Add an Employee",
+          "Update an Employee Role",
+          "Exit"
+        ]
+      },
+    ])
+    .then(({choice}) => {
+      if (choice === "Exit") {
+        return true;
+      }
+      if (choice === "View Departments") {
+        return connection.query("SELECT * FROM department")
+          .then(displayResults)
+      }
+      if (choice === "View Employees") {
+        return connection.query("SELECT * FROM employees")
+          .then(displayResults)
+      }
+      if (choice === "View Roles") {
+        return connection.query("SELECT * FROM Roles")
+          .then(displayResults)
+      }
+      
+    })
+    .then((exit) => {
+      if (!exit) {
+        return promptLoop(connection);
+      }
+    })
+}
+
+const displayResults = ([results]) => {
+  console.table(results);
+}
