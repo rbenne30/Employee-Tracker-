1c84f2b53bee12b2c43544d4952d7b18b9a5f5f2
rbenne30 <ryannebennett1@outlook.com>
2022-06-02 16:21:20 -0400

    added more promises

--- index.js
+++ index.js
@@ -1,37 +1,14 @@
-require('dotenv').config()
-require('console.table')
+require('dotenv').config();
+require('console.table');
 const inquirer = require('inquirer')
 const mysql = require('mysql2/promise')
-const connection = require('./config/connection')
-mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-})
-  .catch((err) => {
-    console.error(err)
-  })
-// .then((connection) => {
-//   if (!connection) {
-//     return;
-//   }
-//   console.log("Connected to database");
-//   promptLoop(connection)
-//     .then(() => {
-//       connection.end();
-//     })
-//     .catch((err) => {
-//       console.error(err)
-//       connection.end();
-//     })
-// })
-// .then(() => {
-//   console.log("Thank you for using this program");
-// })
 
-const promptLoop = () => {
-  inquirer
+const displayResults = ([results]) => {
+  console.table(results);
+}
+
+const promptLoop = (connection) => {
+  return inquirer
     .prompt([
       {
         name: "choice",
@@ -39,12 +16,12 @@ const promptLoop = () => {
         message: "What would you like to do?",
         choices: [
           "View Departments",
-          "View Role",
-          "View Employee",
+          "View Roles",
+          "View Employees",
           "Add a Department",
           "Add a role",
           "Add an Employee",
-          "Update an Employee Role",
+          "Update an Employee",
           "Exit"
         ]
       },
@@ -54,74 +31,197 @@ const promptLoop = () => {
         return true;
       }
       if (choice === "View Departments") {
-        viewDepartments()
+        return connection
+          .query(queries.getDepartments())
           .then(displayResults)
       }
-      if (choice === "View Employee") {
-        viewEmployee()
+      if (choice === "View Roles") {
+        return connection
+          .query(queries.getRoles())
           .then(displayResults)
       }
-      if (choice === "View Role") {
-        viewRole()
+      if (choice === "View Employees") {
+        return connection
+          .query(queries.getEmployees())
           .then(displayResults)
       }
       if (choice === "Add a Department") {
-        inquirer.prompt([
-          {
-            name: "name",
-            message: "What is the name of the department?"
-          }
-        ]).then(results => {
-          let department = results
-          console.log(department)
-          connection.query("INSERT INTO department SET ?", department)
-        })
+        return inquirer
+          .prompt([
+            {
+              name: "name",
+              message: "What is the name of the department?"
+            }
+          ])
+          .then(queries.addDepartment)
+          .then(query => connection.query(...query));
       }
       if (choice === "Add a Role") {
-        addARole()
-          .then(displayResults)
+        return connection
+          .query(queries.getDepartments())
+          .then(([departments]) => {
+            return inquirer
+              .prompt([
+                {
+                  type: "list",
+                  name: "department_id",
+                  message: "Which department is this role a part of?",
+                  choices: departments.map(({ id, name }) => ({ name, value: id }))
+                },
+                {
+                  type: "input",
+                  name: "title",
+                  message: "What is the title of this role?"
+                },
+                {
+                  type: "number",
+                  name: "salary",
+                  message: "What is the salary of this role?"
+                }
+              ])
+              .then(queries.addRole)
+              .then((query) => connection.query(...query))
+          });
       }
       if (choice === "Add an Employee") {
-        addAnEmployee()
-          .then(displayResults)
+        return Promise.all([
+          connection.query(queries.getRoles()),
+          connection.query(queries.getEmployees()),
+        ]).then(([[roles], [employees]]) => {
+          return inquirer
+            .prompt([
+              {
+                type: "list",
+                name: "role_id",
+                message: "Which role does this employee have?",
+                choices: roles.map(({ id, title }) => ({ name: title, value: id }))
+              },
+              {
+                type: "input",
+                name: "first_name",
+                message: "What is the title of this role?"
+              },
+              {
+                type: "number",
+                name: "last_name",
+                message: "What is the salary of this role?"
+              },
+              {
+                type: "list",
+                name: "manager_id",
+                message: "Which department is this role a part of?",
+                choices: [
+                  ...employees.map(({ id, first_name, last_name }) => ({ name: first_name + " " + last_name, value: id })),
+                  { name: "None", value: null }
+                ]
+              },
+            ])
+            .then(queries.addEmployee)
+            .then((query) => connection.query(...query));
+        })
       }
-      if (choice === "Add a Department") {
-        addADepartment()
-          .then(displayResults)
-      }
-      if (choice === "Update an Employee Role") {
-        updateAnEmployeeRole()
-          .then(displayResults)
-      }
-    })
+      if (choice === "Update an Employee") {
+        return Promise.all([
+          connection.query(queries.getRoles()),
+          connection.query(queries.getEmployees()),
+        ]).then(([[roles], [employees]]) => {
+          return inquirer
+            .prompt([
+              {
+                type: "list",
+                name: "id",
+                message: "Which role does this employee have?",
+                choices: employees.map(({ id, first_name, last_name }) => ({ name: first_name + " " + last_name, value: id }))
+              },
+              {
+                type: "list",
+                name: "role_id",
+                message: "Which role does this employee have?",
+                choices: roles.map(({ id, title }) => ({ name: title, value: id }))
+              },
+              {
+                type: "input",
+                name: "first_name",
+                message: "What is the title of this role?"
+              },
+              {
+                type: "number",
+                name: "last_name",
+                message: "What is the salary of this role?"
+              },
+              {
+                type: "list",
+                name: "manager_id",
+                message: "Which department is this role a part of?",
+                choices({ id: employeeId }) {
+                  return [
+                    ...employees
+                      .filter(({ id }) => id !== employeeId)
+                      .map(({ id, first_name, last_name }) => ({ name: first_name + " " + last_name, value: id })),
+                    { name: "None", value: null }
+                  ]
+                }
+              },
+            ])
+            .then(queries.updateEmployee)
+            .then((query) => connection.query(...query));
+        },
     .then((exit) => {
-      if (!exit) {
-        return promptLoop(connection);
+      if (exit) {
+        return;
       }
+      return promptLoop(connection);
     })
+  }
+
+const queries = {
+  getDepartments() {
+    return "SELECT * FROM department";
+  },
+  getRoles() {
+    return "SELECT * FROM role";
+  },
+  getEmployees() {
+    return "SELECT * FROM employee";
+  },
+  addDepartment({ name }) {
+    return ["INSERT INTO department SET ?", { name }]
+  },
+  addRole({ title, salary, department_id }) {
+    return ["INSERT INTO role SET ?", { title, salary, department_id }]
+  },
+  addEmployee({ first_name, last_name, role_id, manager_id }) {
+    return ["INSERT INTO role SET ?", { first_name, last_name, role_id, manager_id }]
+  },
+  updateEmployee({ id, first_name, last_name, role_id, manager_id }) {
+    return ["UPDATE role SET ? WHERE ?", [{ first_name, last_name, role_id, manager_id }, { id }]]
+  }
 }
-function viewDepartments() {
-  return connection.promise().query("SELECT * FROM department")
-}
-function viewEmployee() {
-  return connection.promise().query("SELECT * FROM employee")
-}
-function viewRole() {
-  return connection.promise().query("SELECT * FROM role")
-}
-function addARole() {
-  return connection.promise().query("SELECT * FROM role")
-}
-function addAnEmployee() {
-  return connection.promise().query("SELECT * FROM role")
-}
-function addADepartment() {
-  return connection.promise().query("SELECT * FROM role")
-}
-function updateAnEmployeeRole() {
-  return connection.promise().query("SELECT * FROM role")
-}
-const displayResults = ([results]) => {
-  console.table(results);
-}
-promptLoop()
+
+mysql
+  .createConnection({
+    host: process.env.DB_HOST ?? "localhost",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  })
+  .catch((err) => {
+    console.error(err)
+  })
+  .then((connection) => {
+    if (!connection) {
+      return;
+    }
+    console.log("Connected to database");
+    return promptLoop(connection)
+      .then(() => {
+        connection.end();
+      })
+      .catch((err) => {
+        console.error(err)
+        connection.end();
+      });
+  })
+  .then(() => {
+    console.log("Thank you for using this program");
+  });
